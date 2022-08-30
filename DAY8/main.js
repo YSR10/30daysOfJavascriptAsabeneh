14146caa250a7dfe949151e00df40319d9f594a7
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2022-08-30 21:13:10 +0100

    feat: DAY8

---
+++ DAY8/main.js
@@ -0,0 +1,17 @@
+//1 Create an empty object called dog
+const dog = {};
+
+//2 Print the the dog object on the console
+console.log(dog);
+
+//3 Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
+dog = {
+  name: bingo,
+  legs: 4,
+  color: brown,
+  age: 2,
+  bark: woof,
+};
+getBark: function() {
+   return '${this.bark}'
+}
